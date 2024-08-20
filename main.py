a2e96b13ac2ac2e58ee8368452d8864efae6ebe4
zeonzy <zandvliet_leon@hotmail.com>
2024-08-21 01:13:56 +0200

    ability to move player added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import *
 
 def main():
+    #initialise
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -16,14 +17,27 @@ def main():
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+
+    #main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        #screen.fill(pygame.Color(0,0,0))
+            
         screen.fill("black")
-        player.draw(screen)
+
+        for update in updatable:
+            update.update(dt)
+        for draw in drawable:
+            draw.draw(screen)
+
+
+
+        
+        
+        
         pygame.display.flip()
 
         #limit to 60 fps
