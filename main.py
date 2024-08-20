04cd7f54397214cc8c15aa9605c7814cc46c8fdf
zeonzy <zandvliet_leon@hotmail.com>
2024-08-20 23:49:18 +0200

    start of player class

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+
+import pygame # type: ignore
 from constants import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    #dt = delta time
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill(pygame.Color(0,0,0))
+        #screen.fill(pygame.Color(0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit to 60 fps
+        dt = clock.tick(60) / 1000
 
         
 
---
+++ player.py
@@ -0,0 +1,23 @@
+from circleshape import *
+from constants import *
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.position = pygame.Vector2(x, y)
+        self.rotation = 0
+        self.radius = PLAYER_RADIUS
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+        pass
+
